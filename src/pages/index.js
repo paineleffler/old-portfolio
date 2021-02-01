452fe9eec7748ab0fc31177e77dfb0c11af76811
Paine Leffler <pleffler@vols.utk.edu>
2021-01-31 21:08:49 -0600

    added react-images for the lightbox to see my other screenshots.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
-import * as React from "react"
+import React, { useState } from "react"
 import Img from 'gatsby-image'
 import { graphql } from 'gatsby'
 import tw, { styled } from 'twin.macro'
 import { Element, Link } from 'react-scroll'
+import Carousel, { Modal, ModalGateway } from 'react-images'
 
 import Page from '../components/Page'
 import PaineSVG from '../components/svgs/PaineSVG'
-import GraphqlSVG from '../components/svgs/GraphqlSVG'
-import MongoSVG from '../components/svgs/MongoSVG'
-import NodeSVG from '../components/svgs/NodeSVG'
-import ReactSVG from '../components/svgs/ReactSVG'
+// import GraphqlSVG from '../components/svgs/GraphqlSVG'
+// import MongoSVG from '../components/svgs/MongoSVG'
+// import NodeSVG from '../components/svgs/NodeSVG'
+// import ReactSVG from '../components/svgs/ReactSVG'
 
 const Title = styled.div`
   ${tw`xl:text-6xl text-5xl`};
@@ -68,10 +69,48 @@ const Button = styled.button`
 `
 
 const TechRow = styled.div`
- ${tw`flex flex-1 flex-row justify-around mt-4`};
+ ${tw`flex flex-1 flex-row justify-around mt-6`};
 `
 
+const Captions = {
+  'oc-1.png': 'This is the landing screen for the OurCalling App. It automatically pulls up the resources nearest to you, or in an area you searched. Specific resources can be found in the quick bottom drawer or in the filter section. ',
+  'oc-2.png': 'The bottom drawer allows a vertical scroll list of the search results. Each will navigate to their respective details page.',
+  'oc-3.png': 'By tapping a map pin, the details of the location comes up in a swipe-able card on the bottom of the screen',
+  'oc-4.png': 'The filter screen offers finer search criteria by category, age, gender, or common qualifications.',
+  'oc-5.png': 'By tapping on a resource card or list item, you will be able to see details on the resource and be able to directly call, navigate, or view their webpage.',
+  'oc-6.png': 'On the Identify tab, we allow users to submit information on where help is needed. This can be self identified or by a community member. The goal is for an outreach team to view the submission and bring assistance.',
+  'oc-7.png': 'On the side drawer we can quickly access primary tabs and additional secondary pages like the hotline numbers and settings page.',
+  'wishlist-light-1.png': 'The home page for Wishlist, it allows for public board searching and board creation if it does not exist. (light)',
+  'wishlist-light-2.png': 'This is a profile page on Wishlist. Here you can see the boards you have created and the boards you have favorited. (light)',
+  'wishlist-light-3.png': 'This is a board that was created to manage some of my reminders and to-do items. They have custom tags and correlate to different unrelated projects that I am working on. (light)',
+  'wishlist-light-4.png': 'This is the side drawer of the board. This allows general board configuration for tags, permissions, email notifications, and other preferences. (light)',
+  'wishlist-dark-1.png': 'The home page for Wishlist, it allows for public board searching and board creation if it does not exist. (dark)',
+  'wishlist-dark-2.png': 'This is a profile page on Wishlist. Here you can see the boards you have created and the boards you have favorited. (dark)',
+  'wishlist-dark-3.png': 'This is a board that was created to manage some of my reminders and to-do items. They have custom tags and correlate to different unrelated projects that I am working on. (dark)',
+  'wishlist-dark-4.png': 'This is the side drawer of the board. This allows general board configuration for tags, permissions, email notifications, and other preferences. (dark)',
+  'openspace-1.png': 'This is the homepage of OpenSpace, it goes into details about what the goals of the application serves, and allows users to sign up for meetups to learn more.',
+  'openspace-2.png': 'The OpenSpace profile page allows people to showcase their projects and open-source activity.',
+  'openspace-3.png': 'This is a project details page where you can find project activity and how to start contributing.',
+  'openspace-4.png': 'This is a blog on OpenSpace that allows knowledge sharing and showcasing work with in depth descriptions and discussions.'
+}
+
 export default function IndexPage ({ data }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [activeView, setActiveView] = useState([])
+  
+  function stringCompare (a, b) {
+    if (a < b) {
+      return -1
+    }
+    else {
+      return 1
+    }
+  }
+
+  const wishlistList = data.wishlistImages.edges.map(({ node: { fluid: { src, originalName }} }) => { return { source: src, caption: Captions[originalName], originalName } }).sort((a, b) => stringCompare(a.originalName, b.originalName))
+  const ocList = data.ocImages.edges.map(({ node: { fluid: { src, originalName }} }) => { return { source: src, caption: Captions[originalName], originalName } }).sort((a, b) => stringCompare(a.originalName, b.originalName))
+  const openspaceList = data.openspaceImages.edges.map(({ node: { fluid: { src, originalName }} }) => { return { source: src, caption: Captions[originalName], originalName } }).sort((a, b) => stringCompare(a.originalName, b.originalName))
+
   return (
     <Page>
       <ContentContainer>
@@ -124,12 +163,13 @@ export default function IndexPage ({ data }) {
               <Title>Wishlist <span role="img" aria-label="sparkles">✨</span></Title>
               <Subtitle>A collaborative productivity web application.</Subtitle>
               <Paragraph>Wishlist was built out of a necessity of a place for do-ers to organize personal items or project work without creating additional overhead. My main goal was to allow people to use a process that worked for them, instead of trying to teach or show them ONE way to do project management. Wishlist was designed and developed by me from scratch in the beginning. Later, it was inter open-sourced and grew in features and contributors.</Paragraph>
-              <TechRow>
-                <ReactSVG/>
-                <NodeSVG />
-                <GraphqlSVG />
-                <MongoSVG />
-              </TechRow>
+              <Button 
+                onClick={() => {
+                  setActiveView(wishlistList)
+                  setModalIsOpen(true)
+                }}>
+                  See more
+              </Button>
             </ProjectTextContainer>
           </Row>
         </FlexContainerFull>
@@ -141,15 +181,16 @@ export default function IndexPage ({ data }) {
               <Title>OpenSpace <span role="img" aria-label="person on laptop">🧑🏻‍💻</span></Title>
               <Subtitle>An inter open-source platform for innovation.</Subtitle>
               <Paragraph>OpenSpace was built as a knowledge sharing and collaboration platform to change the idea of assignment based work and learning. OpenSpace gives more code visibility to employees of the company. We really wanted this to be the place to build an internal portfolio and give people an opportunity to lead and teach others.</Paragraph>
-              <TechRow>
-                <ReactSVG/>
-                <NodeSVG />
-                <GraphqlSVG />
-                <MongoSVG />
-              </TechRow>            
+              <Button 
+                onClick={() => {
+                  setActiveView(openspaceList)
+                  setModalIsOpen(true)
+                }}>
+                  See more
+              </Button>
             </ProjectTextContainer>
             <ScreenshotContainer>
-              <Img fluid={data.openspaceMain.childImageSharp.fluid} alt="OpenSpace Home Page Screenshot" />
+              <Img fluid={data.openspaceMain.childImageSharp.fluid} alt="OpenSpace Home Page Screenshot" />     
             </ScreenshotContainer>
           </Row>
         </FlexContainerFull>
@@ -164,10 +205,13 @@ export default function IndexPage ({ data }) {
               <Title>OurCalling <span role="img" aria-label="tent">⛺</span></Title>
               <Subtitle>A mobile resource to support our homeless neighbors.</Subtitle>
               <Paragraph>This mobile app was created in conjunction with <a href="https://www.ourcalling.org/" target="_blank" rel="noreferrer noopener">OurCalling</a>, an organization based in Dallas, TX. The mobile app was focused on connecting people in need with 8 main resource types. In addition to connecting people to resources, we enabled people to donate and help identify people in need by location so that an outreach team could bring assistance.</Paragraph>
-              <TechRow>
-                <ReactSVG/>
-                <GraphqlSVG />
-              </TechRow>
+              <Button 
+                onClick={() => {
+                  setActiveView(ocList)
+                  setModalIsOpen(true)
+                }}>
+                  See more
+              </Button>
             </ProjectTextContainer>
           </Row>
         </FlexContainerFull>
@@ -175,6 +219,13 @@ export default function IndexPage ({ data }) {
           <Paragraph>Made with lots of <span role="img" aria-label="coffee">☕</span></Paragraph>
         </Footer>
       </ContentContainer>
+      <ModalGateway>
+        { modalIsOpen &&
+          <Modal onClose={() => { setModalIsOpen(false) }}>
+            <Carousel views={activeView} activeIndex={0}/>
+          </Modal>
+        }
+      </ModalGateway>
     </Page>
   )
 }
@@ -201,7 +252,7 @@ export const mobileScreenshot = graphql`
 
 export const query = graphql`
   query {
-    wishlistMain: file(relativePath: { eq: "wishlist-3-light.png" }) {
+    wishlistMain: file(relativePath: { eq: "wishlist-light-3.png" }) {
       ...desktopScreenshot
     }
     openspaceMain: file(relativePath: { eq: "openspace-1.png" }) {
@@ -209,6 +260,36 @@ export const query = graphql`
     }
     ocMain: file(relativePath: { eq: "oc.png" }) {
       ...desktopScreenshot
+    }
+    wishlistImages: allImageSharp(filter: {fluid: {src: {regex: "/wishlist/"}}}) {
+      edges {
+        node {
+          fluid(maxWidth: 1512, maxHeight: 953) {
+            originalName
+            src
+          }
+        }
+      }
+    }
+    ocImages: allImageSharp(filter: {fluid: {src: {regex: "/oc-/"}}}) {
+      edges {
+        node {
+          fluid(maxWidth: 312, maxHeight: 672) {
+            originalName
+            src
+          }
+        }
+      }
+    }
+    openspaceImages: allImageSharp(filter: {fluid: {src: {regex: "/openspace/"}}}) {
+      edges {
+        node {
+          fluid(maxWidth: 1512, maxHeight: 953) {
+            originalName
+            src
+          }
+        }
+      }
     }
   }
 `
