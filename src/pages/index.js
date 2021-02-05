852b5f7f682c85cca78b1614b9b5c86bc9e203ff
Paine Leffler <pleffler@vols.utk.edu>
2021-02-05 09:19:33 -0600

    css bugs on layout. height -> min-height and extra margin on buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ const ContentContainer = styled.div`
 `
 
 const FlexContainerFull = styled.div`
-  ${tw`flex flex-row justify-center items-center h-screen`};
+  ${tw`flex flex-row justify-center items-center min-h-screen`};
 `
 
 const Footer = styled.div`
@@ -65,7 +65,7 @@ const ContactLink = styled.a`
 `
 
 const Button = styled.button`
-  ${tw`font-sans relative py-2 px-4 mt-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+  ${tw`font-sans relative py-2 px-4 my-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
 `
 
 const TechRow = styled.div`
